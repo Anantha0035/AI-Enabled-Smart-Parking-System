e728fc9e51aa0ee64a3f7a8ff32595d1db461e34
Anantha <anantha.20201CSE0035@presidencyuniversity.in>
2024-01-15 23:16:12 +0530

    Final Commit

---
+++ app/src/main/java/com/aienabledparking/carparking/home/userdetail.java
@@ -0,0 +1,16 @@
+package com.aienabledparking.carparking.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aienabledparking.carparking.R;
+
+public class userdetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userdetail);
+    }
+}
